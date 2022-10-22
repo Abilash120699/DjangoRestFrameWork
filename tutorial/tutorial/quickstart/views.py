1bee5c4031863194a50c87d808ac0b425eea6b2f
abilsashk <abilash.kumar@impigertech.com>
2022-10-22 11:06:05 +0530

    quickstart

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User,Group
+from .serializers import UserSerializer,GroupSerializer
 # Create your views here.
+
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission = [permissions.IsAuthenticated]
+
+class GroupViewset(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission = [permissions.IsAuthenticated]
